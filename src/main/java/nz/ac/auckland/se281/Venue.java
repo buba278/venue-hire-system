c06246bd9cba332248a08adbe4b32c4e953bd228
buba278 <chrismotta2005@gmail.com>
2024-03-24 15:42:32 +1300

    implemented the printVenues method for all 4 main scenarios

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -2,15 +2,28 @@ package nz.ac.auckland.se281;
 
 public class Venue {
   private String name;
-  private int code;
+  private String code;
   private int capacity;
   private int hireFee;
 
   // Handle paramters as strings due to HireSystem formatting
   public Venue(String name, String code, String capacity, String hireFee) {
     this.name = name;
-    this.code = Integer.valueOf(code);
+    this.code = code;
     this.capacity = Integer.valueOf(capacity);
     this.hireFee = Integer.valueOf(hireFee);
+  }
+
+  public String getName() {
+    return this.name;
+  }
+  public String getCode() {
+    return this.code;
+  }
+  public int getCapacity() {
+    return this.capacity;
+  }
+   public int getHireFee() {
+    return this.hireFee;
   }
 }
--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -6,16 +6,42 @@ import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
+  // list of venues 
   private ArrayList<Venue> venues = new ArrayList<Venue>();
+  // numbers to words for printing
+  private final String[] numbers = new String[]{"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; 
 
   public VenueHireSystem() {}
 
   public void printVenues() {
     // TODO implement this method
+
     // Check if the venue list is empty
     if (venues.size() == 0) {
       MessageCli.NO_VENUES.printMessage();
       return;
+    }
+    // if only one entry
+    else if (venues.size() == 1) {
+      MessageCli.NUMBER_VENUES.printMessage("is", this.numbers[1], "");
+      Venue venue = venues.get(0);
+      MessageCli.VENUE_ENTRY.printMessage(venue.getName(), venue.getCode(), String.valueOf(venue.getCapacity()), String.valueOf(venue.getHireFee()));
+    }
+
+    // less than 10 entries
+    else if (venues.size() < 10) {
+      MessageCli.NUMBER_VENUES.printMessage("are", this.numbers[venues.size()], "s");
+      for (Venue i : venues) {
+        MessageCli.VENUE_ENTRY.printMessage(i.getName(), i.getCode(), String.valueOf(i.getCapacity()), String.valueOf(i.getHireFee()));
+      }
+    }
+
+    // 10 or more entries
+    else if (venues.size() >= 10) {
+      MessageCli.NUMBER_VENUES.printMessage("are", String.valueOf(venues.size()), "s");
+      for (Venue i : venues) {
+        MessageCli.VENUE_ENTRY.printMessage(i.getName(), i.getCode(), String.valueOf(i.getCapacity()), String.valueOf(i.getHireFee()));
+      }
     }
   }
 
